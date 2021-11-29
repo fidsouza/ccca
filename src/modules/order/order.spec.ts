aae71c3cb599884d57dedfc2c2b3a3226c42cec8
Filipe <filipe.cdesouza@gmail.com>
2021-11-28 23:27:22 -0300

    test: adjust test order

--- src/modules/order/order.spec.ts
+++ src/modules/order/order.spec.ts
@@ -1,11 +1,12 @@
 import Order from './order';
+
 const desc = 'any description';
 const price = 100;
 const quantity = 1;
+const validCpf = '619.443.000-15';
 
 describe('Order tests', () => {
   test('should be a order when three itens and valid cpf', () => {
-    const validCpf = '619.443.000-15';
     const newOrder = new Order(validCpf);
     newOrder.addItem(desc, price, quantity);
     newOrder.addItem(desc, price, quantity);
@@ -17,14 +18,12 @@ describe('Order tests', () => {
     expect(() => new Order(invalidCpf).cpf).toThrow(new Error('Invalid CPF'));
   });
   test('should be a order with discount', () => {
-    const validCpf = '619.443.000-15';
     const discountinPercent = 10;
     const newOrder = new Order(validCpf, discountinPercent);
     newOrder.addItem(desc, price, quantity);
     expect(newOrder.getTotal()).toBe(90);
   });
   test('should be a order without discount', () => {
-    const validCpf = '619.443.000-15';
     const newOrder = new Order(validCpf);
     newOrder.addItem(desc, price, quantity);
     expect(newOrder.getTotal()).toBe(100);
